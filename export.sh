52a53d004b0792a3f2871f38042de72176473615
Niklas Reimer <niklas@backbord.net>
2022-03-07 20:18:40 +0100

    fix password

--- export.sh
+++ export.sh
@@ -4,7 +4,7 @@ mkdir -p /tmp
 
 while true; do
 
-    wget ${TVH_URL}/xmltv/channels --user=${TVH_USER} --password=${TVH_USER} -O /tmp/iptvGuide.xml
+    wget ${TVH_URL}/xmltv/channels --user=${TVH_USER} --password=${TVH_PASSWORD} -O /tmp/iptvGuide.xml
     sleep 15
     xmlstarlet ed -u "//programme/@start" -x "concat(substring(., 0, 9), substring(string(1000000 + substring(., 9, 6)), 2))" -u "//programme/@stop" -x "concat(substring(., 0, 9), substring(string(1000000 + substring(., 9, 6)), 2))" /tmp/iptvGuide.xml > /output/iptvGuide.xml
     rm /tmp/iptvGuide.xml

0d17f79ec23483a57436f4f55fbbb302936722fd
Niklas Reimer <niklas@backbord.net>
2022-03-07 20:27:54 +0100

    output folder

--- export.sh
+++ export.sh
@@ -6,7 +6,7 @@ while true; do
 
     wget ${TVH_URL}/xmltv/channels --user=${TVH_USER} --password=${TVH_PASSWORD} -O /tmp/iptvGuide.xml
     sleep 15
-    xmlstarlet ed -u "//programme/@start" -x "concat(substring(., 0, 9), substring(string(1000000 + substring(., 9, 6)), 2))" -u "//programme/@stop" -x "concat(substring(., 0, 9), substring(string(1000000 + substring(., 9, 6)), 2))" /tmp/iptvGuide.xml > /output/iptvGuide.xml
+    xmlstarlet ed -u "//programme/@start" -x "concat(substring(., 0, 9), substring(string(1000000 + substring(., 9, 6)), 2))" -u "//programme/@stop" -x "concat(substring(., 0, 9), substring(string(1000000 + substring(., 9, 6)), 2))" /tmp/iptvGuide.xml > /iptvGuide.xml
     rm /tmp/iptvGuide.xml
 
     sleep ${INTERVAL}
